24adbb93d824084554452831fe5e6ec17663d6a6
Tamas D. Nagy <tamas.daniel.nagy@irob.uni-obuda.hu>
2019-02-28 13:47:53 +0100

    Impedance control tests

---
+++ irob_subtask_logic/scripts/wheel.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+# Author: Anton Deguet
+# Date: 2017-07-22
+
+# (C) Copyright 2017 Johns Hopkins University (JHU), All Rights Reserved.
+
+# --- begin cisst license - do not edit ---
+
+# This software is provided "as is" under an open source license, with
+# no warranty.  The complete license can be found in license.txt and
+# http://www.cisst.org/cisst/license.txt.
+
+# --- end cisst license ---
+
+# Start a single arm using
+# > rosrun dvrk_robot dvrk_console_json -j <console-file>
+
+# To communicate with the arm using ROS topics, see the python based example dvrk_arm_test.py:
+# > rosrun dvrk_python dvrk_mtm_cartesian_impedance <arm-name>
+
+import dvrk
+import math
+import sys
+import rospy
+import numpy
+import PyKDL
+import threading
+from sensor_msgs.msg import Joy
+from cisst_msgs.msg import prmCartesianImpedanceGains
+
+# example of application using arm.py
+class example_application:
+
+    # configuration
+    def configure(self, robot_name):
+        print rospy.get_caller_id(), ' -> configuring dvrk_arm_test for ', robot_name
+        self.arm = dvrk.mtm(robot_name)
+        self.coag_event = threading.Event()
+        rospy.Subscriber('/dvrk/footpedals/coag',
+                         Joy, self.coag_event_cb)
+        self.set_gains_pub = rospy.Publisher(self.arm._arm__full_ros_namespace + '/set_cartesian_impedance_gains',
+                                             prmCartesianImpedanceGains, latch = True, queue_size = 1)
+
+    # homing example
+    def home(self):
+        print rospy.get_caller_id(), ' -> starting home'
+        self.arm.home()
+        # get current joints just to set size
+        goal = numpy.copy(self.arm.get_current_joint_position())
+        # go to zero position
+        goal.fill(0)
+        self.arm.move_joint(goal, interpolate = True)
+
+    # foot pedal callback
+    def coag_event_cb(self, data):
+        if (data.buttons[0] == 1):
+            self.coag_event.set()
+
+    # wait for foot pedal
+    def wait_for_coag(self):
+        self.coag_event.clear()
+        self.coag_event.wait(600)
+
+
+    # tests
+    def tests(self):
+        gains = prmCartesianImpedanceGains()
+        # set orientation to identity quaternions
+        gains.ForceOrientation.w = 1.0
+        gains.TorqueOrientation.w = 1.0
+
+        print rospy.get_caller_id(), ' -> press COAG pedal to set straight position'
+        self.wait_for_coag()
+        radius = 0.3
+
+        currpos = self.arm.get_current_position()
+
+        center = numpy.array([currpos.p[0], currpos.p[1], currpos.p[2]])
+        center = center - numpy.array([0.0, radius, 0.0])
+
+
+
+
+
+
+        gains.PosStiffNeg.x = -200.0;
+        gains.PosStiffPos.x = -200.0;
+        gains.PosDampingNeg.x = -5.0;
+        gains.PosDampingPos.x = -5.0;
+        gains.ForcePosition.x = self.arm.get_current_position().p[0]
+
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+          print rospy.get_caller_id(), ' -> Refresh gains'
+          currpos = self.arm.get_current_position()
+          pos = numpy.array([currpos.p[0], currpos.p[1], currpos.p[2]])
+          dir = pos - center
+          dir = dir / numpy.linalg.norm(dir)
+          gains.PosStiffNeg.z = -200.0 * dir[2];
+          gains.PosStiffPos.z = -200.0 * dir[2];
+          gains.PosDampingNeg.z = -5.0 * dir[2];
+          gains.PosDampingPos.z = -5.0 * dir[2];
+
+          gains.PosStiffNeg.y = -200.0 * dir[1];
+          gains.PosStiffPos.y = -200.0 * dir[1];
+          gains.PosDampingNeg.y = -5.0 * dir[1];
+          gains.PosDampingPos.y = -5.0 * dir[1];
+
+          target = center + (dir * radius)
+
+          gains.ForcePosition.y = target[1]
+          gains.ForcePosition.z = target[2]
+
+          self.set_gains_pub.publish(gains)
+          rate.sleep()
+
+
+
+
+    # main method
+    def run(self):
+        self.home()
+        self.tests()
+
+if __name__ == '__main__':
+    try:
+        if (len(sys.argv) != 2):
+            print sys.argv[0], ' requires one argument, i.e. MTML or MTMR'
+        else:
+            application = example_application()
+            application.configure(sys.argv[1])
+            application.run()
+
+    except rospy.ROSInterruptException:
+        pass
